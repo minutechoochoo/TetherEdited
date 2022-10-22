fba77f4f94b4328640253f0caa66d510c3158546
minutechoochoo <88225111+minutechoochoo@users.noreply.github.com>
2022-10-21 19:43:54 -0600

    Add files via upload

---
+++ TetherSE/GetTargetedBlock.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Game.Entity.UseObject;
+using VRageMath;
+
+
+namespace TetherSE
+{
+    public class GetTargetedBlock
+    {
+        public static void GetPort()
+        {
+            IMyUtilities utils = MyAPIGateway.Utilities;      
+            MySlimBlock block;
+            var localplayer = MySession.Static.LocalHumanPlayer;
+
+            var caster = MyAPIGateway.Session.Player.Character?.EquippedTool?.Components?.Get<MyCasterComponent>();
+            block = caster?.HitBlock;
+
+            if(selectedBlock != null)
+            {
+                MyVisualScriptLogicProvider.SetHighlightLocal(selectedBlock.Name, -1, 0, Color.White, MySession.Static.LocalPlayerId, null);
+            }
+ 
+            
+            if(block != null && block.FatBlock is IMyTerminalBlock)
+            {
+                var terminalBlock = block.FatBlock as IMyTerminalBlock;
+                var useObjectBase = terminalBlock.Components.Get<MyUseObjectsComponentBase>();
+
+                List<IMyUseObject> useableObjects = new List<IMyUseObject>();
+                useObjectBase.GetInteractiveObjects(useableObjects);
+                var owned = false;
+
+                switch (localplayer.GetRelationTo(terminalBlock.OwnerId))
+                {
+                    case MyRelationsBetweenPlayerAndBlock.NoOwnership:
+                        owned = true;
+                        break;
+                    case MyRelationsBetweenPlayerAndBlock.Owner:
+                        owned = true;
+                        break;
+                    case MyRelationsBetweenPlayerAndBlock.FactionShare:
+                        owned = true;
+                        break;
+                    case MyRelationsBetweenPlayerAndBlock.Neutral:
+                        owned = true;
+                        break;
+                    case MyRelationsBetweenPlayerAndBlock.Enemies:
+                        break;
+                    case MyRelationsBetweenPlayerAndBlock.Friends:
+                        break;
+                }
+                if (!owned)
+                {
+                    utils.ShowNotification("You do not own this block... Nice try.", 5000, "Red");
+                    return;
+                }
+
+                foreach (var useableObject in useableObjects)
+                {
+                    if (useableObject.PrimaryAction == UseActionEnum.OpenInventory)
+                    {
+                        utils.ShowNotification($"Tethered to {terminalBlock.CustomName}", 5000, "White");
+                        selectedBlock = terminalBlock;
+                        selectedObject = useableObject;
+
+                        terminalBlock.Name = terminalBlock.EntityId.ToString();
+                        MyEntities.SetEntityName((MyEntity)terminalBlock);
+                        MyVisualScriptLogicProvider.SetHighlightLocal(terminalBlock.Name, 2, 3, Color.ForestGreen, MySession.Static.LocalPlayerId, null);
+
+                        return;
+                    }
+                }
+            }
+
+            utils.ShowNotification("Block doesn't exist or does not have a inventory access port.", 5000, "Red");   
+        }
+
+        public static IMyUseObject selectedObject;
+        public static IMyTerminalBlock selectedBlock;
+    }
+}
---
+++ TetherSE/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HarmonyLib;
+using VRage.Plugins;
+
+
+namespace TetherSE
+{
+    public class Plugin : IPlugin, IDisposable
+    {
+        public void Dispose()
+        {
+           
+
+        }
+
+        public void Init(object gameInstance)
+        {
+         
+            Reflections.Initialize();
+            
+        }
+
+        public void Update()
+        {
+
+            Tether.Update();    
+            
+        }
+
+
+    }
+}
+
+
